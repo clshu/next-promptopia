eb9dcde5a40a193b09786228ece759ce8a450516
clshu <clshu2020@gmail.com>
2023-07-27 22:20:55 -0700

    Fix import alias bug. Cerate .env and create Home page and RootLayout.

---
+++ app/page.jsx
@@ -0,0 +1,18 @@
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text_center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient">AI-Powered Prompts</span>
+      </h1>
+      <p className="desct text_center">
+        Promtopia is a an open-source AI prompting tool for modern world to
+        discover, create a nd sahre creative prompts.
+      </p>
+      {/* Feed */}
+    </section>
+  )
+}
+
+export default Home
