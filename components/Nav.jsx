3945781292086764fb7142d5957db644efcf63bb
clshu <clshu2020@gmail.com>
2023-07-27 23:26:09 -0700

    Set up Nav component. Desktop Navigation first.

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,7 +1,75 @@
+'use client';
 import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import { useState, useEffect } from 'react';
+import { signIn, signOut, useSession, getProvider } from 'next-auth/react';
 
 const Nav = () => {
-  return <div>Nav</div>;
+  const [provider, setProvider] = useState(null);
+  const isUserLoggedin = true;
+
+  useEffect(() => {
+    const setProvider = async () => {
+      const provider = await getProvider();
+      setProvider(provider);
+    };
+    setProvider();
+  }, []);
+
+  return (
+    <nav className="flex-between w-full mb-16 pt-3">
+      <Link href="/" className="flex gap-2 flex-center">
+        <Image
+          src="/assets/images/logo.svg"
+          alt="Promptopia Logo"
+          width={30}
+          height={30}
+          className="object-contain"
+        />
+        <p className="logo_text">Promtopia</p>
+      </Link>
+      {/* Desktop sNavigation */}
+      <div className="sm:flex hidden">
+        {isUserLoggedin ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className="black_btn">
+              Create Prompt
+            </Link>
+            <button type="button" onClick={signOut} className="outline_btn">
+              Sign Out
+            </button>
+
+            <Link href="/profile">
+              <Image
+                src="/assets/images/logo.svg"
+                alt="Profile"
+                width={37}
+                height={37}
+                className="rounded-full"
+              />
+            </Link>
+          </div>
+        ) : (
+          <>
+            {provider &&
+              Object.values(provider).map((provider) => (
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className="black_btn"
+                >
+                  Sign In with {provider.name}
+                </button>
+              ))}
+          </>
+        )}
+      </div>
+      {/* Mobile Navigation */}
+      <div className="sm:hidden flex relative"></div>
+    </nav>
+  );
 };
 
 export default Nav;
