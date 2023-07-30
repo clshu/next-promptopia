237a7a6fca4704ea28659a9c8542b217bb497a07
clshu <clshu2020@gmail.com>
2023-07-29 19:11:25 -0700

    1. Implement PromptCard component to show user image, username and user email. 2. Also show prompt and tags and make tags clickable. Add copy button and copy prompt to clipboard. 3. Start to us pnpm instead of npm. Delete package-lock.json and add pnpm-lock.yaml.

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,7 +1,62 @@
-import React from 'react';
+'user client';
 
-const PromptCard = () => {
-  return <div>PromptCard</div>;
+import { useState } from 'react';
+import Image from 'next/image';
+import { useRouter, usePathname } from 'next/navigation';
+import { useSession } from 'next-auth/react';
+
+const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+  const [copied, setCopied] = useState('');
+
+  const handleCopy = () => {
+    setCopied(post.prompt);
+    navigator.clipboard.writeText(post.prompt);
+    setTimeout(() => {
+      setCopied('');
+    }, 3000);
+  };
+
+  return (
+    <div className="prompt_card">
+      <div className="flex justify-between item-start gap-5">
+        <div className="flex-1 flex justify-start items-center gap3 cursor-pointer">
+          <Image
+            src={post.creator.image}
+            alt={post.creator.username}
+            width={40}
+            height={40}
+            className="rounded-full"
+          />
+        </div>
+        <div className="flex flex-col">
+          <h3 className="font-satoshi font-semibold tex-gray-900">
+            {post.creator.username}
+          </h3>
+          <p className="font-inter text-sm text-gray-900">
+            {post.creator.email}
+          </p>
+        </div>
+        <div className="copy_btn" onClick={handleCopy}>
+          <Image
+            src={
+              copied === post.prompt
+                ? '/assets/icons/tick.svg'
+                : '/assets/icons/copy.svg'
+            }
+            width={12}
+            height={12}
+          />
+        </div>
+      </div>
+      <p className="my-4 font-satoshi tex-sm text-gray-700">{post.prompt}</p>
+      <p
+        className="font-inter text-sm blue_gradient cursor-pointer"
+        onClick={() => handleTagClick && handleTagClick(post.tag)}
+      >
+        {post.tag}
+      </p>
+    </div>
+  );
 };
 
 export default PromptCard;
